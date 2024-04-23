957b19c4a5279331de4e63f0e38156f9113faa73
Khaled Obaid <Khalid.AlShikhSaleh@cubesplatform.com>
2024-04-24 02:10:14 +0300

    -+ refControl page

--- src/app/presentation/components/ref-controller/index.vue
+++ src/app/presentation/components/ref-controller/index.vue
@@ -6,10 +6,11 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
+let numArr = [1, 2, 3, 4, 5, 6]
 const controlledValue = ref('hello!')
 
 const yelled = (msg: string) => {
   controlledValue.value = msg
 }
-defineExpose({ yelled })
+defineExpose({ yelled, numArr })
 </script>
--- src/app/presentation/pages/ref-page/index.vue
+++ src/app/presentation/pages/ref-page/index.vue
@@ -2,7 +2,10 @@
   <div>
     <ref-controller ref="refGate"></ref-controller>
   </div>
-  <v-button @click="forceToChange()">Change from parent</v-button>
+  <div>
+    <v-button @click="forceToChange()">Change from parent</v-button>
+  </div>
+  <v-button @click="showNumArray()">alertMe</v-button>
 </template>
 
 <script setup lang="ts">
@@ -10,5 +13,7 @@ import { defineAsyncComponent, ref } from 'vue'
 const refGate = ref()
 
 const forceToChange = () => refGate?.value.yelled('Hello gentle men!')
+const showNumArray = () => alert(refGate.value.numArr)
+
 const RefController = defineAsyncComponent(() => import('../../components/ref-controller/index.vue'))
 </script>
